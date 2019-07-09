5ede431a5f69d2913044d964f14744e2f0b83815
Courtney Dyson <courtneydyson87@gmail.com>
2019-07-09 23:55:51 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,7 @@
 # +require 'pry'
 
 def load_library(emoticon_file)
-  emoticons = YAML.load_file('./lib/empticons.yml')
+  emoticons = YAML.load_file('./lib/emoticons.yml')
   emoticon_lib = {'get_meaning' => {}, 'get_emoticon' => {} }
   emoticons.each do |meaning, value|
     english = value[0]
